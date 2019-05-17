6b42e6914233949b77ac05d38009faae14f42ffb
Adam Nizol <adam@nizol.ca>
2019-05-17 15:21:43 -0700

    add single page turn functionality

--- src/components/Book.vue
+++ src/components/Book.vue
@@ -1,11 +1,13 @@
 <template>
   <div :class="$style['bookContainer']">
     <div :class="$style['book']">
-      <button :class="$style['turnPgLeft']" @click="turnPage('left')"> Turn left</button>
-      <button :class="$style['turnPgRight']" @click="turnPage('right')">Turn right</button>
+      <div :class="$style['topMenu']">
+        <img src="https://i.imgur.com/6lD73vx.png" :class="$style['turnPgRight']" @click="turnPage('right')">
+        <img src="https://i.imgur.com/43ePFH7.png" :class="$style['turnPgLeft']" @click="turnPage('left')">
+      </div>
       <div :class="$style['pages']">
-        <template>
-          <div :class="$style['page']">
+        <template v-for="(page, pageInd) in pages">
+          <div :class="$style['page']" ref="pageRef">
           </div>
         </template>
       </div>
@@ -14,21 +16,34 @@
 </template>
 
 <script>
-
+  //const Velocity = require('velocity-animate').default;
   export default {
     components: {
 
     },
 
     data() {
+      let pageList = [1, 2, 3]
       return {
-
+        pages: pageList
       }
     },
 
     methods: {
       turnPage(direction = 'left'){
-        console.log("turning the page "+direction)
+        console.log("turning the page "+direction);
+        let p = this.$refs.pageRef[0];
+        switch (direction){
+          case "left":
+            Velocity(p,"stop");
+            Velocity(p,{rotateY: "-180deg"}, 500);
+            break;
+          case "right":
+            Velocity(p,"stop");
+            Velocity(p,{rotateY: "0deg"}, 500);
+            break;
+        }
+
       }
     },
     mounted() {
@@ -40,13 +55,15 @@
 
 <style module lang="less">
   .bookContainer{
-    width: 80%;
+    position: absolute;
+    width: 100%;
     height: 100%;
     display: flex;
     align-items: center;
     justify-content: center;
   }
   .book{
+    position: absolute;
     background-color: #2d1300;
     border-radius: 7px;
     width: 800px;
@@ -54,21 +71,38 @@
     border: 3px solid #0d0700;
   }
   .pages{
-    position: relative;
+    position: absolute;
     left: 50%;
-    width: 45%;
-    height: 90%;
+    width: 48%;
+    height: 92%;
     top: 5%;
+    perspective: 1500px;
   }
   .page{
     box-sizing: border-box;
-    position: relative;
+    position: absolute;
     width: 100%;
     background-color: white;
     height: 100%;
-    transform: perspective(800px) rotateY(-0deg);
+    transform: rotateY(-0deg);
     transform-origin: 0% 50%;
-    border-right: 2px solid rgba(0,0,0,1);
     border-left: 6px solid rgba(0,0,0,0.3);
+  }
+  .topMenu{
+    box-sizing: border-box;
+    display: absolute;
+    top: 0;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 0px 5px 0px 5px;
+    >img{
+      margin: 0;
+      height: 20px;
+    }
+    >img:hover{
+      cursor: pointer;
+    }
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import Velocity from 'velocity-animate'
 
 Vue.config.productionTip = false
+
+//window.$ = require('velocity-animate');
+
 
 new Vue({
   store,
