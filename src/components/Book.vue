fbdb5baeef2089796a529a55deccf6b28485d441
Adam Nizol <adam@nizol.ca>
2019-05-17 13:26:37 -0700

    bring in html and css from proof of concept

--- src/components/Book.vue
+++ src/components/Book.vue
@@ -1,7 +1,16 @@
 <template>
-    <div id="healthBar" >
-        <div class="healthBarStatus"></div>
+  <div :class="$style['bookContainer']">
+    <div :class="$style['book']">
+      <button :class="$style['turnPgLeft']" @click="turnPage('left')"> Turn left</button>
+      <button :class="$style['turnPgRight']" @click="turnPage('right')">Turn right</button>
+      <div :class="$style['pages']">
+        <template>
+          <div :class="$style['page']">
+          </div>
+        </template>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -18,31 +27,48 @@
     },
 
     methods: {
-
+      turnPage(direction = 'left'){
+        console.log("turning the page "+direction)
+      }
     },
     mounted() {
-      
+
     }
 
   }
 </script>
 
-<style>
-  #healthBar{
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    width: 300px;
-    height: 20px;
-    background-color: rgba(0,0,0,0.45);
-    border: 2px solid rgba(0,0,0,1);
-    border-radius: 0px 5px 5px 0px;
-  }
-  .healthBarStatus{
-    width: 40%;
+<style module lang="less">
+  .bookContainer{
+    width: 80%;
     height: 100%;
-    border-radius: 0px 5px 5px 0px;
-    background-color: red;
-
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .book{
+    background-color: #2d1300;
+    border-radius: 7px;
+    width: 800px;
+    height: 500px;
+    border: 3px solid #0d0700;
+  }
+  .pages{
+    position: relative;
+    left: 50%;
+    width: 45%;
+    height: 90%;
+    top: 5%;
+  }
+  .page{
+    box-sizing: border-box;
+    position: relative;
+    width: 100%;
+    background-color: white;
+    height: 100%;
+    transform: perspective(800px) rotateY(-0deg);
+    transform-origin: 0% 50%;
+    border-right: 2px solid rgba(0,0,0,1);
+    border-left: 6px solid rgba(0,0,0,0.3);
   }
 </style>
