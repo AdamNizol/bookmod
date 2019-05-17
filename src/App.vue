fa5fc7d891c02cf4bdd8ceb18187844af8c2226b
Adam Nizol <adam@nizol.ca>
2019-05-17 13:01:26 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Book></Book>
+  </div>
+</template>
+
+<script>
+import Book from './components/Book.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Book
+  }
+}
+</script>
+
+<style lang="less">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Book.vue
@@ -0,0 +1,48 @@
+<template>
+    <div id="healthBar" >
+        <div class="healthBarStatus"></div>
+    </div>
+</template>
+
+<script>
+
+  export default {
+    components: {
+
+    },
+
+    data() {
+      return {
+
+      }
+    },
+
+    methods: {
+
+    },
+    mounted() {
+      
+    }
+
+  }
+</script>
+
+<style>
+  #healthBar{
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 300px;
+    height: 20px;
+    background-color: rgba(0,0,0,0.45);
+    border: 2px solid rgba(0,0,0,1);
+    border-radius: 0px 5px 5px 0px;
+  }
+  .healthBarStatus{
+    width: 40%;
+    height: 100%;
+    border-radius: 0px 5px 5px 0px;
+    background-color: red;
+
+  }
+</style>
